e73bf860908bfa1f79dbe4ee2a1b929f48da78b5
Hedying <2969837242@qq.com>
2025-09-11 15:30:26 +0800

    画了一堆

--- okay.R
+++ okay.R
@@ -10,3 +10,17 @@ data_filtered <- data[data$age > 10, ]
 # 保留math等于1的样本
 df_math <- data_filtered[data_filtered$Math == 1, ]
 
+
+library(AER)
+data(STAR) 
+library(ggplot2) 
+p1<-ggplot(STAR, aes(readk)) + 
+  geom_histogram(bins=30,colour="black",fill="blue")
+p2<-ggplot(STAR, aes(x=gender,y=readk)) + 
+  geom_boxplot() +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  xlab("Reading Score") +
+  ylab("Count")
+library(gridExtra) 
+grid.arrange(p1,p2,ncol = 2, nrow = 1)
