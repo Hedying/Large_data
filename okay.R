704bc1893c7113d0168a498e5ff2296a6914ba0a
Hedying <2969837242@qq.com>
2025-09-11 11:09:10 +0800

    宝宝数学成绩好

--- okay.R
+++ okay.R
@@ -6,3 +6,7 @@ data <- read_dta("C:/Users/Lenovo/Desktop/大二下/计量经济学/大作业/20
 
 #只保留大于十岁的样本
 data_filtered <- data[data$age > 10, ]
+
+# 保留math等于1的样本
+df_math <- data_filtered[data_filtered$Math == 1, ]
+
